57312ecbcc4b20dbdb9c2f5fbf4e5d2df5cd766f
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2021-11-15 15:32:40 +0800

    Update main.py
    pprint真好用

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import apache_log_parser
 
-line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"")
-# line_parser = apache_log_parser.make_parser("%h <<%P>> %t %Dus \"%r\" %>s %b  \"%{Referer}i\" \"%{User-Agent}i\" %l %u")
+apache_log_line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"")
 
-data='162.158.90.242 - - [01/Nov/2021:00:30:29 +0800] "GET / HTTP/1.1" 200 1325 "https://weeklyosm.eu/de/archives/14940" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"'
-# data='127.0.0.1 <<6113>> [16/Aug/2013:15:45:34 +0000] 1966093us "GET / HTTP/1.1" 200 3478  "https://example.com/" "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.18)" - -'
+# data = open("access.log20211011", "r").readlines()
+data = open("access.log", "r").readlines()
+# data='162.158.90.242 - - [01/Nov/2021:00:30:29 +0800] "GET / HTTP/1.1" 200 1325 "https://weeklyosm.eu/de/archives/14940" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"'
 
-log_line_data = line_parser(data)
+log_line_data = apache_log_line_parser(data[70])
+
 from pprint import pprint
 pprint(log_line_data)
+
+# print(log_line_data)
