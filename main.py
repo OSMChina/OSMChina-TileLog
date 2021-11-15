998f140dccb4c913caeb5bbf6f90f3321607181a
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2021-11-15 16:02:56 +0800

    Update main.py
    只留下需要的信息

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import apache_log_parser
+from pprint import pprint
 
-apache_log_line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"")
+apache_log_parser_line = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"")
 
 # data = open("access.log20211011", "r").readlines()
 data = open("access.log", "r").readlines()
-# data='162.158.90.242 - - [01/Nov/2021:00:30:29 +0800] "GET / HTTP/1.1" 200 1325 "https://weeklyosm.eu/de/archives/14940" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"'
 
-log_line_data = apache_log_line_parser(data[70])
-
-from pprint import pprint
-pprint(log_line_data)
-
-# print(log_line_data)
+# for i in range(len(data)):
+for i in range(4):
+    data_token = apache_log_parser_line(data[i])
+    # pprint(data_token)
+    data_dict = {}
+    data_dict["basic-ip"] = data_token["remote_host"]
+    data_dict["basic-time"] = data_token["time_received_tz_datetimeobj"]
+    data_dict["basic-url"] = data_token["request_url"]
+    data_dict["basic-traffic"] = data_token["bytes_tx"]
+    data_dict["http-ver"] = "HTTP" + data_token["request_http_ver"]
+    data_dict["http-method"] = data_token["request_method"]
+    data_dict["http-status"] = data_token["status"]
+    data_dict["http-referer"] = data_token["request_header_referer"]
+    data_dict["ua-full"] = data_token["request_header_user_agent"]
+    data_dict["ua-mobile"] = data_token["request_header_user_agent__is_mobile"]
+    data_dict["ua-browser"] = data_token["request_header_user_agent__browser__family"] + "/" + data_token[
+        "request_header_user_agent__browser__version_string"]
+    data_dict["ua-os"] = data_token["request_header_user_agent__os__family"] + "/" + data_token[
+        "request_header_user_agent__os__version_string"]
+    pprint(data_dict)
