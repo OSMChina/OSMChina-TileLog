460c9e8ca14beff9793d667f41f0019f1a498cc6
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2021-11-15 15:16:27 +0800

    Update main.py
    复制/etc/apache2/apache2.conf 里面的格式进来
    就好了

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import apache_log_parser
 
-line_parser = apache_log_parser.make_parser("%h - - %t \"%r\" %>s %b  \"%{Referer}i\" \"%{User-Agent}i\"")
+line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"")
 # line_parser = apache_log_parser.make_parser("%h <<%P>> %t %Dus \"%r\" %>s %b  \"%{Referer}i\" \"%{User-Agent}i\" %l %u")
 
 data='162.158.90.242 - - [01/Nov/2021:00:30:29 +0800] "GET / HTTP/1.1" 200 1325 "https://weeklyosm.eu/de/archives/14940" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"'
